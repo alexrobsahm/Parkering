7ced928ed4fd8122f2cf0539ea6aa9dab915c668
Alex Robsahm <alex.robsahm@athega.se>
2016-09-02 11:27:02 +0200

    Added welcome message

--- app/src/main/java/com/robsahm/parking/Settings.java
+++ app/src/main/java/com/robsahm/parking/Settings.java
@@ -1,6 +1,9 @@
 package com.robsahm.parking;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.SharedPreferences;
 import android.preference.PreferenceFragment;
 import android.os.Bundle;
 
@@ -10,6 +13,24 @@ public class Settings extends Activity {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+
+        SharedPreferences sharedPreferences = getSharedPreferences("PREFERENCE", MODE_PRIVATE);
+
+        boolean isFirstTime = sharedPreferences.getBoolean("isFirstTime", true);
+        if (isFirstTime){
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setTitle(R.string.welcome_header);
+            builder.setMessage(R.string.welcome_text);
+            builder.setNeutralButton(R.string.welcome_button, new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialogInterface, int i) {
+                    dialogInterface.cancel();
+                }
+            });
+            builder.create().show();
+
+            sharedPreferences.edit().putBoolean("isFirstTime", false).apply();
+        }
     }
 
     @Override
